0df256fc3fcb938b03841a88fb3a271257b5cdf0
Chrisdu44 <christopherguichard44@gmail.com>
2022-11-10 11:06:32 +0100

    Express 04bis - Valider la saisie utilisateur - correction

--- app.js
+++ app.js
@@ -22,19 +22,15 @@ const { validateUser } = require("./validators.js");
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.post("/api/users", validateUser, userHandlers.postUser);
 
-app.put("/api/movies", validateMovie, movieHandlers.postMovie);
-app.put("/api/users", validateUser, userHandlers.postUser);
+app.put("/api/movies", validateMovie, movieHandlers.updateMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users", validateUser, userHandlers.updateUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", userHandlers.postUser);
-
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", userHandlers.updateUser);
 
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", userHandlers.deleteUser);
